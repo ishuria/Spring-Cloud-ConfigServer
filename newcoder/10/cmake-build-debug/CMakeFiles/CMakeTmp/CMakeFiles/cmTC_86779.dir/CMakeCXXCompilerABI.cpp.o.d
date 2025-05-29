bb8f0e80e476ebc3e8d5f42e571edf2f65f5730b
xiangchaolei <xiangchaolei@bytedance.com>
2025-05-29 21:03:28 +0800

    Revert "1"
    This reverts commit c42f6877c13368c643422aef406a37e2cc3b058a.

--- newcoder/10/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_86779.dir/CMakeCXXCompilerABI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cmTC_86779.dir/CMakeCXXCompilerABI.cpp.o: \
-  /Applications/CLion.app/Contents/bin/cmake/mac/share/cmake-3.21/Modules/CMakeCXXCompilerABI.cpp \
-  /Applications/CLion.app/Contents/bin/cmake/mac/share/cmake-3.21/Modules/CMakeCompilerABI.h
--- newcoder/migong/cmake-build-debug/CMakeFiles/migong.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for migong.
-# This may be replaced when dependencies are built.
